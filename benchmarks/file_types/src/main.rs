5918aea822fe0feec5f19bc54f653dcf8389df17
archsolar <m_charizard@hotmail.nl>
2022-09-11 05:36:18 +0200

    patch

--- benchmarks/file_types/src/main.rs
+++ benchmarks/file_types/src/main.rs
@@ -2,7 +2,7 @@
 
 // use core::time;
 
-use ne_app::{get_time_passed, App};
+use ne_app::{get_time_passed, App, World, Schedule};
 use ne_render::{AppExit, OnRedrawRequested, OnWindowCloseRequested, RenderPlugin, WindowSettings};
 
 fn main() {
@@ -12,8 +12,12 @@ fn main() {
     ne_bench::size_of::print_size_of::<i128>();             //type: i128, size: 16
     ne_bench::size_of::print_size_of::<&str>();             //type: &str, size: 16
     ne_bench::size_of::print_size_of::<String>();           //type: alloc::string::String, size: 24    
-    ne_bench::size_of::print_size_of::<Vec<String>>();      //type: alloc::vec::Vec<alloc::string::String>, size: 24      
-    ne_bench::size_of::print_size_of::<WindowSettings>();   //type: ne_render::WindowSettings, size: 128      
-    ne_bench::size_of::print_size_of::<RenderPlugin>();     //type: alloc::vec::Vec<alloc::string::String>, size: 24      
+    ne_bench::size_of::print_size_of::<Vec<String>>();      //type: alloc::vec::Vec<alloc::string::String>, size: 24
 
+    ne_bench::size_of::print_size_of::<WindowSettings>();   //type: ne_render::WindowSettings, size: 128
+    ne_bench::size_of::print_size_of::<RenderPlugin>();     //type: ne_render::RenderPlugin, size: 0
+    ne_bench::size_of::print_size_of::<App>();              //type: ne_app::App, size: 808
+    ne_bench::size_of::print_size_of::<World>();            //type: bevy_ecs::world::World, size: 632
+    ne_bench::size_of::print_size_of::<Schedule>();         //type: bevy_ecs::schedule::Schedule, size: 112
+    
 }
