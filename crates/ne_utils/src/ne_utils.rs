488f510bf0341ed0a1ca08c68cd00ed3e100f3d6
archsolar <m_charizard@hotmail.nl>
2022-09-18 05:41:58 +0200

    removed unnecessary crates

--- crates/ne_utils/src/ne_utils.rs
+++
@@ -1,8 +0,0 @@
-use ahash::RandomState;
-pub use hashbrown;
-
-/// A [`HashMap`][hashbrown::HashMap] implementing aHash, a high
-/// speed keyed hashing algorithm intended for use in in-memory hashmaps.
-///
-/// aHash is designed for performance and is NOT cryptographically secure.
-pub type HashMap<K, V> = hashbrown::HashMap<K, V, RandomState>;
